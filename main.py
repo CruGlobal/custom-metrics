6f8d9ef3ed0f17a12330d3d1f128e555ec3ba802
roguisharcanetrickster <213383044+roguisharcanetrickster@users.noreply.github.com>
2025-06-10 14:46:08 +0700

    wip

--- main.py
+++ main.py
@@ -8,7 +8,14 @@ import os
 PROMETHEUS_URL = os.getenv("PROMETHEUS_URL", "http://prometheus:9090")
 BQ_PROJECT_ID = os.getenv("BQ_PROJECT_ID")
 BQ_DATASET_ID = os.getenv("BQ_DATASET_ID", "internet_metrics")
-BQ_TABLE_ID = os.getenv("BQ_TABLE_ID", "uptime")
+BQ_TABLE_ID_up = os.getenv("BQ_TABLE_ID", "uptime")
+BQ_TABLE_ID_speed = os.getenv("BQ_TABLE_ID", "speedtest")
+metrics = [
+    "scrape_duration_seconds",
+    "scrape_samples_scraped",
+    "scrape_series_added",
+    "node_netstat_TcpExt_SyncookiesFailed"
+]
 
 # BigQuery Credentials (loaded from mounted secret)
 credentials = service_account.Credentials.from_service_account_file(
@@ -25,24 +32,59 @@ def query_prometheus(query):
 def parse_and_format(result_json):
     """Extracts desired fields from Prometheus response and prepares rows for BigQuery."""
     results = result_json.get("data", {}).get("result", [])
-    rows = []
+    values = []
     ts = int(time.time())
     for r in results:
         metric = r.get("metric", {})
         value = r.get("value", [None, None])[1]
-        rows.append({
+        values.append({
             "timestamp": ts,
             "metric": f"{metric.get('job', 'unknown')}_{metric.get('instance', 'unknown')}_up",
             "site_id": metric.get("site_id", "Isaac's house"),
             "location": metric.get("location", "Thailand"),
             "instance": metric.get("instance"),
             "job": metric.get("job"),
-            "value": f"{value}"
-        })
-    return rows
+            "data": r,
+            "value": f"{str(value)}"
+        })  
+    sample = results[0] if results else {}
+    if not sample:
+        print("No results found in Prometheus response.")
+        return {}
+    row = [ {
+            "timestamp": ts,
+            "metric": "up",
+            "site_id": (sample.get(0, {}).get("metric", {}).get("site_id", "Isaac's house")),
+            "location": sample.get(0, {}).get("metric", {}).get("location", "Thailand"),
+            "instance": sample.get(0, {}).get("metric", {}).get("instance"),
+            "job": "prometheus",
+            "value": f"{str(results)}"
+        }]
+    return row
 
-def push_to_bigquery(rows):
-    table_id = f"{BQ_PROJECT_ID}.{BQ_DATASET_ID}.{BQ_TABLE_ID}"
+def parse_speed(result_json, metric):
+    """Extracts desired fields from Prometheus response and prepares rows for BigQuery."""
+    results = result_json.get("data", {}).get("result", [])
+    values = []
+    ts = int(time.time())
+    for r in results:
+        value = r.get("value", [None, None])[1]
+        values.append({
+            "timestamp": ts,
+            "metric": metric,
+            "data": r,
+            "value": f"{str(value)}"
+        })  
+    row = [ {
+            "timestamp": ts,
+            "metric": "speed",
+            "job": "prometheus",
+            "value": f"{str(results)}"
+        }]
+    return row
+
+def push_to_bigquery(rows, table_id=None):
+    table_id = f"{BQ_PROJECT_ID}.{BQ_DATASET_ID}.{table_id}"
     errors = bq_client.insert_rows_json(table_id, rows)
     if errors:
         print("Encountered errors while inserting rows:", errors)
@@ -53,11 +95,27 @@ def main():
     print("Pulling metrics from Prometheus...")
     prom_data = query_prometheus("up")
     rows = parse_and_format(prom_data)
+    speed = []
+    for m in metrics:
+        res = query_prometheus(m)
+        for r in res.get("data", {}).get("result", []):
+            metric = r["metric"]
+            val = r["value"][1]
+            print(f"{metric.get('job', '')}@{metric.get('instance', '')} → {val}")
+        speed.append( (res, m) )
     if rows:
         print(f"Pushing {len(rows)} rows to BigQuery...")
-        push_to_bigquery(rows)
+        push_to_bigquery(rows, BQ_TABLE_ID_up)
     else:
-        print("No data found.")
+        print("No uptime data found.")
+    if speed:
+        print(f"Pushing {len(rows)} speed rows to BigQuery...")
+        for s in speed:
+            rows = parse_speed(s[0], s[1])
+            if rows:
+               push_to_bigquery(rows, BQ_TABLE_ID_speed)
+            else:
+                print("No speed data found.")
 
 if __name__ == "__main__":
     main()
